87e7dd95641e9e447a618d515ca1ded5312abab3
Windows 10 <Windows 10@PC03>
2019-07-02 09:19:21 +0700

    4321

---
+++ Lesson3/ParameterBinding.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+
+namespace Lesson3
+{
+    [Transaction(TransactionMode.Manual)]
+   public class ParameterBinding : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Application app = uiapp.Application;
+            Document doc = uidoc.Document;
+            ParameterRe parameter = new ParameterRe(uiapp);
+
+            //for column architecter
+            parameter.CreateParamerterRe("group_first", "parameter_name1", BuiltInCategory.OST_Columns);
+
+            return Result.Succeeded;
+        }
+    }
+}
--- Lesson3/ParameterRe.cs
+++ Lesson3/ParameterRe.cs
@@ -8,6 +8,7 @@ using Autodesk.Revit.ApplicationServices;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.UI.Selection;
+using System.IO;
 
 namespace Lesson3
 {
@@ -54,6 +55,73 @@ namespace Lesson3
                 return true;
             }
             catch (Exception ex) { return false; }
+        }
+
+
+        //Create ShareParameter
+       public void CreateParamerterRe(string groupName, string parameterName, BuiltInCategory category)
+        {
+            Application app = _uiApp.Application;
+            Document doc = _uiApp.ActiveUIDocument.Document;
+            DefinitionFile defitionFile = app.OpenSharedParameterFile();          
+            string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "ShareParameterArmo.txt");
+            if (defitionFile == null)
+            {
+                StreamWriter stream;
+                stream = new StreamWriter(path);
+                stream.Close();
+                app.SharedParametersFilename = path;                             
+                defitionFile = app.OpenSharedParameterFile();
+            }
+            using(Transaction t = new Transaction(doc,"Create shareparameter"))
+            {
+                t.Start();
+                //set paramneter
+                SetNewParameterToInstance(defitionFile, category, groupName, parameterName);
+                t.Commit();
+            }  
+
+        }
+
+        //add parameter
+        public bool SetNewParameterToInstance(DefinitionFile myDefitionfile, BuiltInCategory category,string groupName,string parameter)
+        {
+            try
+            {
+                DefinitionGroups myGroups = myDefitionfile.Groups;
+                DefinitionGroup myGroup = null;
+                Definition myDefination_ProductDate=null;
+                foreach(var item in myGroups)
+                {
+                    if (item.Name == groupName)
+                    {
+                        myGroup = item;
+                        myDefination_ProductDate = item.Definitions.Single();
+                        break;
+                    }
+                }
+                if (myGroup == null)
+                {
+                    myGroup = myGroups.Create(groupName);
+                    ExternalDefinitionCreationOptions option = new ExternalDefinitionCreationOptions(parameter, ParameterType.Text);
+                    myDefination_ProductDate = myGroup.Definitions.Create(option);
+                }
+
+                CategorySet categorySet = _uiApp.Application.Create.NewCategorySet();
+                Category myCategory = _uiApp.ActiveUIDocument.Document.Settings.Categories.get_Item(category);
+                categorySet.Insert(myCategory);
+                InstanceBinding instantBinding = _uiApp.Application.Create.NewInstanceBinding(categorySet);
+                BindingMap bindingMap = _uiApp.ActiveUIDocument.Document.ParameterBindings;
+
+                bool instanceBindOk=bindingMap.Insert(myDefination_ProductDate, instantBinding,BuiltInParameterGroup.PG_TEXT);
+
+                return instanceBindOk;
+  
+            }
+            catch(Exception ex)
+            {
+                return false;
+            }
         }
 
 
